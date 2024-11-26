734682efa8a8acd88a17788cbbd4c9f998c77980
EvelinFerreiraAraujo <144921034+EvelinFerreiraAraujo@users.noreply.github.com>
2024-11-26 18:32:06 -0300

    Update perguntas.js
    descrição das perguntas

--- perguntas.js
+++ perguntas.js
@@ -7,13 +7,13 @@ criaCartao(
 criaCartao(
     'Custo da Viagem', 
     'Qual foi o custo total dessa viagem por pessoa e qual a modalidade de aquisição?', 
-    'Informar o valor na moeda original do local e o valor convertido para R$. Informar se a aquisição foi via agência de viagens ou aquisição de forma independente '
+    'Na viagem independente sai no valor de R$ 17.517 e a viagem através de pacotes fica em torno de R$ 10.071.Então, acaba sendo mais em conta ir através do uso de pacotes fornecidos por sites de viagens nesse caso.'
 )
 
 criaCartao(
     'Fato Curioso', 
     'Uma curiosidade sobre cidade/ponto turístico/país cisitado!', 
-    'Informar através de um breve texto uma curiosidade.'
+    ' A cidade abriga o Instituto Ricardo Brennand, um dos maiores acervos privados de arte e história do Brasil. Localizado em um castelo no bairro da Várzea, o instituto possui uma vasta coleção de armas, armaduras pinturas e esculturas, além de um museu que preserva a história de Pernambuco e do Brasil de maneira única.'
 )
 
 criaCartao(
